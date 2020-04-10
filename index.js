294f9a2f487ae592ccab1cc68b11dd4271491b5c
Courtney Zahner <cfzahner@gmail.com>
2020-04-10 17:03:49 -0500

    Add examples of objects

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-// create a variable timeOfDay and assign it a string
-const timeOfDay = "Evening";
+const myObj = { cat: "232", myString: "izzy" };
 
-// use timeOfDay in the template literal to create a message like "Good morning!" or "Good evening!"
-const greeting = `Good ${timeOfDay}`;
-
-// print greeting
-console.log(greeting);
-
-// create a new variable, but do not assign it a value
-let x;
-
-// print the variable and its type
-console.log(x);
-
-// assign the variable a value that indicates the variable is purposely blank
-x = null;
-
-// print the variable and its type again
-console.log(null);
-
-// try to print a variable that does not exist
-console.log(a);
+console.log(myObj["myString"]);
+myObj.myString;
