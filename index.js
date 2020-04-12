46d3b7d0dfaae0c067d4efbc04a5fff2151283dd
Courtney Zahner <cfzahner@gmail.com>
2020-04-12 13:19:43 -0500

    Answered operator and if problems

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const myArray = ["Courtney", 29, "brown hair", true];
+// modulo (remainder)
+console.log(4 % 2);
+console.log(5 % 2);
+console.log(6 % 2);
+console.log(7 % 2);
 
-console.log(myArray[0]);
-console.log(myArray[1]);
-console.log(myArray[2]);
-console.log(myArray[3]);
-
-myArray[1] = Courtney;
-console.log(me.name);
+console.log(6 % 3);
+console.log(7 % 3);
+console.log(8 % 3);
+console.log(9 % 3);
