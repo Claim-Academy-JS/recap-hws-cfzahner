fdedafb858e1d00ef021e6acda4ef60323024b10
Courtney Zahner <cfzahner@gmail.com>
2020-04-20 12:56:01 -0500

    Add countdown method

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ string = string.toUpperCase();
 console.log(string);
 
 const graduation = new Date(`June 26 2020`);
-const start = new Date(Date.now());
+const today = new Date(Date.now());
 
 function showRemaining() {
   let total = graduation - today;
-  return today;
+  let seconds = Math.floor((total / 1000) % 60);
+  let minutes = Math.floor((total / 1000 / 60) % 60);
+  let hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  let days = Math.floor((total / (1000 * 60 * 60 * 24)) % 7);
+  let weeks = Math.floor(total / (1000 * 60 * 60 * 24 * 7));
+
+  let countdownTimer = `${weeks} weeks, ${days} days ${hours} hours, ${minutes} minutes,and ${seconds} seconds`;
+
+  if (total > 0) {
+    return `We have ${countdownTimer} left until we become full stack developers!`;
+  }
+  return "Congratulations! We have completed Claim Academy's bootcamp";
 }
+
+console.log(showRemaining());
